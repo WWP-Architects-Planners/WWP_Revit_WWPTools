dfd29d32a3ef10ecaecb59772cfc50c0784cc53e
Jason <jtian@svn-ap.com>
2026-01-30 21:47:53 -0500

    Update WPF UI toggles and cleanup libs

--- hooks/app-init.py
+++ hooks/app-init.py
@@ -1,6 +1,7 @@
 # import pyrevit libraries
+import os
 import clr
-from pyrevit import forms,script
+from pyrevit import forms, script
 from WWP_msgUtils import *
 
 # check if notifications are disabled
@@ -13,4 +14,20 @@ if msgUtils_muted():
 # icoFile = remPath + r'bin\Graphics\ico256_WWP.ico'
 
 # Display the message to the user
-forms.toast("Toolbar has been loaded!","WWP_Tool",appid="WWP Architects + Planners",actions=None)
+icon_path = os.path.normpath(os.path.join(os.path.dirname(__file__), "..", "lib", "WWPtools-logo.png"))
+if os.path.exists(icon_path):
+    forms.toaster.send_toast(
+        "Toolbar has been loaded!",
+        title="WWP_Tool",
+        appid="WWP Architects + Planners",
+        icon=icon_path,
+        click=None,
+        actions=None,
+    )
+else:
+    forms.toast(
+        "Toolbar has been loaded!",
+        "WWP_Tool",
+        appid="WWP Architects + Planners",
+        actions=None,
+    )
